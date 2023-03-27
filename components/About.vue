3d827de4306a77a517640388b10a0f5808dbc1ec
nandar-dev <mgnandar290@gmail.com>
2023-03-27 18:49:23 +0630

    update

---
+++ components/About.vue
@@ -0,0 +1,53 @@
+<template>
+  <section class="container about" id="about">
+    <h1>Who am I?</h1>
+    <div class="img-content">
+      <img src="./../assets/images/nd.png" alt="" />
+    </div>
+
+    <p>
+      Hello, I'm [Your Name], a passionate and experienced web developer with
+      [Number of years] of experience in creating responsive and user-friendly
+      websites. My expertise lies in front-end web development, and I specialize
+      in using HTML, CSS, and JavaScript to build beautiful and functional
+      websites that engage users and meet business objectives. My passion for
+      web development began [when/how you got interested in web development].
+      <br />
+      <br />
+      Since then, I've had the opportunity to work on a wide range of projects
+      for clients in various industries, from small businesses to large
+      corporations. I'm dedicated to staying up-to-date with the latest trends
+      and technologies in the field, and I enjoy the challenge of solving
+      complex problems and finding innovative solutions. In my free time, I
+      enjoy [hobbies or interests related to web development or technology]. I
+      believe that a strong work-life balance is essential to maintaining
+      creativity and productivity, and I'm always looking for new ways to
+      improve my skills and grow as a developer. If you're looking for a skilled
+      and reliable web developer who is passionate about creating websites that
+      are both beautiful and functional, please feel free to get in touch. I'm
+      always excited to hear about new projects and collaborate with like-minded
+      individuals.
+    </p>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+.about {
+  h1 {
+    text-align: center;
+    font-size: var(--section-title-font-size);
+    color: var(--section-title-color);
+    padding-bottom: 50px;
+  }
+
+  .img-content {
+    display: flex;
+    justify-content: center;
+  }
+  img {
+    width: 300px;
+    padding-bottom: 50px;
+
+  }
+}
+</style>
--- components/Home.vue
+++ components/Home.vue
@@ -5,9 +5,12 @@
       <h1>NANDAR</h1>
       <h2>Web Developer</h2>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum quo nihil
-        eligendi rem animi illum voluptas, maxime illo veniam tempora?
+        I am a passionate web developer with a strong focus on delivering high
+        quality websites that are visually appealing and user friendly. I enjoy
+        taking on new challenges and staying up to date with the latest
+        technologies and trends in the industry.
       </p>
+      <button>Resume</button>
     </div>
     <div class="image">
       <img src="./../assets/images/home-img.svg" alt="Home Image" />
@@ -25,7 +28,6 @@
 
 <style lang="scss">
 .home {
-  padding-top: calc(var(--navbar-height) + 50px);
   height: 100vh;
   display: flex;
   align-items: center;
@@ -54,11 +56,30 @@
     p {
       color: var(--text-grey-color);
     }
+
+    button {
+      margin-top: 20px;
+    }
   }
 
   img {
     width: 100%;
-    min-width: 500px;
+    min-width: 600px;
+  }
+}
+
+.scroll-down {
+  position: absolute;
+  bottom: 4rem;
+  display: flex;
+  text-align: center;
+  justify-content: center;
+  width: 100%;
+  a {
+    display: flex;
+    align-items: center;
+    font-size: 1rem;
+    color: var(--text-color);
   }
 }
 
@@ -66,43 +87,42 @@
   width: 26px;
   height: 40px;
   border-radius: 15px;
-  border: 2px solid #000;
+  border: 2px solid var(--text-grey-color);
   position: relative;
   overflow: hidden;
   margin-top: 40px;
-}
+  .roll {
+    position: absolute;
+    top: 8px;
+    left: 50%;
+    width: 4px;
+    margin-left: -2px;
+    height: 4px;
+    border-radius: 4px;
+    background: var(--text-grey-color);
+    -webkit-animation: mouseScroll 0.8s cubic-bezier(0.7, 0, 0.3, 1) infinite
+      alternate;
+    animation: mouseScroll 0.8s cubic-bezier(0.7, 0, 0.3, 1) infinite alternate;
+    z-index: 2;
+  }
 
-.mouse .roll {
-  position: absolute;
-  top: 8px;
-  left: 50%;
-  width: 4px;
-  margin-left: -2px;
-  height: 4px;
-  border-radius: 4px;
-  background: #000;
-  -webkit-animation: mouseScroll 0.8s cubic-bezier(0.7, 0, 0.3, 1) infinite
-    alternate;
-  animation: mouseScroll 0.8s cubic-bezier(0.7, 0, 0.3, 1) infinite alternate;
-  z-index: 2;
-}
-
-.mouse .rollshadow {
-  position: absolute;
-  top: 8px;
-  left: 50%;
-  width: 4px;
-  margin-left: -2px;
-  height: 4px;
-  background: #aaa;
-  border-radius: 4px;
-  -webkit-animation: mouseScroll 0.8s cubic-bezier(0.7, 0, 0.3, 1) infinite
-    alternate;
-  animation: mouseScroll 0.8s cubic-bezier(0.7, 0, 0.3, 1) infinite alternate;
-  animation-delay: 0s;
-  -webkit-animation-delay: 80ms;
-  animation-delay: 80ms;
-  z-index: 1;
+  .rollshadow {
+    position: absolute;
+    top: 8px;
+    left: 50%;
+    width: 4px;
+    margin-left: -2px;
+    height: 4px;
+    background: #aaa;
+    border-radius: 4px;
+    -webkit-animation: mouseScroll 0.8s cubic-bezier(0.7, 0, 0.3, 1) infinite
+      alternate;
+    animation: mouseScroll 0.8s cubic-bezier(0.7, 0, 0.3, 1) infinite alternate;
+    animation-delay: 0s;
+    -webkit-animation-delay: 80ms;
+    animation-delay: 80ms;
+    z-index: 1;
+  }
 }
 
 @-webkit-keyframes mouseScroll {
@@ -129,23 +149,20 @@
   }
 }
 
-.scroll-down {
-  position: absolute;
-  bottom: 4rem;
-  display: flex;
-  text-align: center;
-  justify-content: center;
-  width: 100%;
-}
+@media screen and (max-width: 768px) {
+  .home {
+    height: unset;
+    margin-top: calc(var(--navbar-height) + 30px);
+    justify-content: start;
 
-.scroll-down a {
-  display: flex;
-  align-items: center;
-  font-size: 1rem;
-  color: var(--text-color);
-}
+    img {
+      min-width: unset;
+      max-width: 600px;
+    }
+  }
 
-.scroll-icon {
-  font-size: 2rem;
+  .scroll-down {
+    display: none;
+  }
 }
 </style>
