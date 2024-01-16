61fbd02af2bb43c5cc534784ea8f0ff93a1903d8
nandar-dev <mgnandar290@gmail.com>
2024-01-16 23:44:38 +0630

    update

--- pages/blog.vue
+++ pages/blog.vue
@@ -15,53 +15,42 @@
     </div>
 
     <div class="content">
-      <img class="cover-img" src="https://images.unsplash.com/photo-1702957954496-8bba5d73a390?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHx8" alt="">
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam eaque
-        non, nemo magni fugit perspiciatis omnis sunt corrupti nam sit, hic
-        earum? Quae a perspiciatis accusantium aut dicta nulla quas, dolore
-        odio! Assumenda dicta dolore eos, sunt ullam eaque, nam recusandae a
-        tempore incidunt unde laudantium expedita magni beatae neque dolorum.
-        Sequi aliquam facere vel fuga id, ea optio tenetur placeat, dolorum
-        maxime veniam excepturi magni temporibus neque hic suscipit pariatur at
-        dignissimos deleniti cumque fugit omnis! Laborum illum neque, animi rem
-        tenetur sit! Incidunt nihil culpa et voluptas totam cupiditate a iure
-        architecto? Laborum ratione tenetur eligendi quibusdam non quod illo
-        ipsum. Nihil tempore animi dolor laboriosam, tenetur cumque commodi
-        sequi expedita itaque ad suscipit facere? Blanditiis maiores sunt autem
-        voluptas porro magni laborum dicta repellendus quibusdam nobis esse iure
-        ea qui, neque, natus vitae officia cupiditate ducimus! Rem, explicabo,
-        omnis autem ducimus pariatur adipisci reprehenderit veritatis officiis
-        quod, aliquam dolores tempora dolore. Nulla totam minus deserunt odio
-        accusamus illo assumenda repellat fugiat atque aspernatur libero
-        architecto in vel itaque accusantium quaerat alias, eligendi perferendis
-        veritatis, magnam excepturi nobis rerum beatae! Fugit tempore error in
-        hic reiciendis earum est consequatur pariatur architecto, iste ea
-        explicabo quis molestias eos et.
-      </p>
-
-      <code>
-        <pre>
-          body {
-          color: yellow;
-          font-size: 16px;
-          line-height: 1.5;
-          }
-          </pre
-        >
-      </code>
+      <img
+        class="cover-img"
+        src="https://images.unsplash.com/photo-1702957954496-8bba5d73a390?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHx8"
+        alt="" />
+      <div v-html="html"></div>
     </div>
   </div>
-  <div id="container">
-            <div id="editor">
-            </div>
-        </div>
 </template>
- 
 
-<style lang="scss" scoped>
+<script>
+export default {
+  data() {
+    return {
+      html: `<h1>Testing</h1><p><br></p><pre class="ql-syntax" spellcheck="false">Hello Wold
+</pre>`,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+.ql-syntax {
+  background-color: #23241f;
+  color: #f8f8f2;
+  overflow: visible;
+
+  white-space: pre-wrap;
+  margin-bottom: 5px;
+  margin-top: 5px;
+  padding: 5px 10px;
+  border-radius: 3px;
+  cursor: text;
+}
+
 .blog-detail {
- 
+    user-select: all;
 
   padding: calc(var(--navbar-height) + 50px) 150px 0 150px;
 
@@ -81,14 +70,12 @@
   .content {
     margin-top: 50px;
 
-    .cover-img{
+    .cover-img {
       max-height: 400px;
       width: 100%;
       object-fit: cover;
       border-radius: var(--base-border-radius);
     }
   }
-
-
 }
 </style>
